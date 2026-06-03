79a2f102df0eb1d40bcb6e7184987a49137f821e
TessaTomy <tessatomy757@gmail.com>
2026-06-03 15:36:49 +0530

    Implemented state updation

--- trial/sam/urls.py
+++ trial/sam/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
    path('', views.sam, name='sam'),
    path('add_state/', views.add_state, name='add_state'),
    path('state_list/', views.state_list, name='state_list'),
+   path('states/<int:pk>/edit/', views.update_state, name='update_state'),  # <-- update path
 ]
--- trial/sam/views.py
+++ trial/sam/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+from urllib import request
+from django.shortcuts import get_object_or_404, redirect, render
 from .forms import StudentForm, DistrictForm, StateForm
 from .models import State, District, Student
 
@@ -19,3 +20,14 @@ def add_state(request):
 def state_list(request):
     states = State.objects.all()
     return render(request, 'sam/state_list.html', {'states': states})
+
+def update_state(request, pk):
+    state=get_object_or_404(State, pk=pk)
+    if request.method == 'POST':
+        form = StateForm(request.POST, instance=state)
+        if form.is_valid():
+            form.save()
+            return redirect('state_list')
+    else:
+        form = StateForm(instance=state)
+    return render(request, 'sam/update_state.html', {'form': form})
